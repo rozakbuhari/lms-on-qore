2734bee017f2d916c112af4cf3a23a2b1f2600ab
Fathurozak Buhari <rozakbuhari@gmail.com>
2021-01-17 23:43:52 +0700

    Remove tailwind purge styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  purge: ["./src/pages/**/*.tsx", "./src/components/**/*.tsx"],
-  darkMode: false, // or 'media' or 'class'
+  purge: false,
+  darkMode: false,
   theme: {
     extend: {},
   },
